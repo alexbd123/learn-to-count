c0148d9ed2541164b237c8c086c4f274e11fc2a7
alexbd123 <alexpjbd@gmail.com>
2024-07-21 15:51:44 +0100

    get count buttons working

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [englishCount, setEnglishCount] = useState({
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+    10: "Ten",
+    isHidden: false
+  });
+
+  const [romanianCount, setRomanianCount] = useState({
+    0: "Zero",
+    1: "Unu",
+    2: "Doi",
+    3: "Trei",
+    4: "Patru",
+    5: "Cinci",
+    6: "șase",
+    7: "șapte",
+    8: "Opt",
+    9: "nouă",
+    10: "Zece",
+    isHidden: false
+  });
+
+  const [currentKey, setKey] = useState(0);
+
+  function hideEnglish() {
+    setEnglishCount(prevCount => ({...prevCount, isHidden: !prevCount.isHidden}))
+  }
+
+  function hideRomanian() {
+    setRomanianCount(prevCount => ({...prevCount, isHidden: !prevCount.isHidden}))
+  }
+
+  function incrementCounts() {
+    if (currentKey < 10) {
+      setKey(prevKey => prevKey + 1);
+    }
+    console.log(currentKey);
+  }
+
+  function decrementCounts() {
+    if (currentKey > 0) {
+      setKey(prevKey => prevKey - 1);
+    }
+    console.log(currentKey);
+  }
 
   return (
     <>
+      <div id='buttons'>
+        <button onClick={decrementCounts}>⬅️</button>
+        <button onClick={incrementCounts}>➡️</button>
+        <button onClick={hideEnglish}>Show/Hide English</button>
+        <button onClick={hideRomanian}>Show/Hide Romanian</button>
+        <button>Random (English)</button>
+        <button>Random (Romanian)</button>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {englishCount.isHidden ? "?" : englishCount[currentKey]}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{romanianCount.isHidden ? "?" : romanianCount[currentKey]}</div>
     </>
   )
 }
